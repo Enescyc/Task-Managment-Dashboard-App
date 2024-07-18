ddf1312e7b3c0c5516c78992498e0036d4f7929a
enescyc <enes.cyclones@gmail.com>
2024-07-18 12:03:36 +0300

    fix(ui): fix build error.

--- components/hero/hero-component.tsx
+++ components/hero/hero-component.tsx
@@ -4,7 +4,7 @@ function HeroComponent() {
   return (
     <div className={"flex flex-col gap-y-2"}>
       <h3 className={"text-[#141522] text-2xl font-semibold"}>Hi, Skylar Dias</h3>
-      <h5 className={"text-[#54577A]"}>Let's finish your task today!</h5>
+      <h5 className={"text-[#54577A]"}>Lets finish your task today!</h5>
     </div>
   );
 }
