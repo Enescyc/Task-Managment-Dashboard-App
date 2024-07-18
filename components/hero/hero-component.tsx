c537192a62fe8b9c32dd1b5b72baa6825a7a63f0
enescyc <enes.cyclones@gmail.com>
2024-07-18 13:39:16 +0300

    feat(ui): Hero component fixed.

--- components/hero/hero-component.tsx
+++ components/hero/hero-component.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import NotificationComponent from "@/components/sidebar/notification-component";
+import ProfileComponent from "@/components/sidebar/profile-component";
 
 function HeroComponent() {
   return (
-    <div className={"flex flex-col gap-y-2"}>
-      <h3 className={"text-[#141522] text-2xl font-semibold"}>Hi, Skylar Dias</h3>
-      <h5 className={"text-[#54577A]"}>Lets finish your task today!</h5>
+    <div className={"flex flex-col lg:flex-row lg:items-center lg:justify-between gap-y-2"}>
+      <div>
+        <h3 className={"text-[#141522] text-2xl font-semibold"}>Hi, Skylar Dias</h3>
+        <h5 className={"text-[#54577A]"}>Lets finish your task today!</h5>
+      </div>
+      <div className={"lg:flex gap-x-2 hidden"}>
+        <NotificationComponent/>
+        <ProfileComponent/>
+      </div>
     </div>
   );
 }
--- components/sidebar/profile-component.tsx
+++ components/sidebar/profile-component.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ProfileHeroIcon from "@/public/icons/ProfileHeroIcon";
+import Image from "next/image";
 
 function ProfileComponent() {
   return (
     <div
       className={"flex flex-col w-11 h-11 border-[#F5F5F7] rounded-full hover:cursor-pointer items-center justify-center"}>
-      <ProfileHeroIcon/>
+      <Image src={"/images/img_1.png"} alt={"profile"} height={100} width={100}/>
     </div>
   );
 }
